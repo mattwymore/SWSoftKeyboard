6ea40268e05a81623ffce1d4958eaa10fe47a672
Spencer Williams <s@spencerenglish.com>
2014-09-04 16:37:26 -0700

    Initial Commit

---
+++ SWSoftKeyboard/SWSoftKeyboard.h
@@ -0,0 +1,13 @@
+//
+//  SWSoftKeyboard.h
+//  SWSoftKeyboard
+//
+//  Created by Spencer Williams on 9/4/14.
+//  Copyright (c) 2014 Spencer Williams. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SWSoftKeyboard : NSObject
+
+@end
---
+++ SWSoftKeyboard/SWSoftKeyboard.m
@@ -0,0 +1,13 @@
+//
+//  SWSoftKeyboard.m
+//  SWSoftKeyboard
+//
+//  Created by Spencer Williams on 9/4/14.
+//  Copyright (c) 2014 Spencer Williams. All rights reserved.
+//
+
+#import "SWSoftKeyboard.h"
+
+@implementation SWSoftKeyboard
+
+@end
