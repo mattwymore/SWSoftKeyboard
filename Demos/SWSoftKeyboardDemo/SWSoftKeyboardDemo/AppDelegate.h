18afb66467454237ecfcded002ad17f9f451105f
Spencer Williams <s@spencerenglish.com>
2014-09-08 11:56:57 -0700

    ok the demo builds now

---
+++ Demos/SWSoftKeyboardDemo/SWSoftKeyboardDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SWSoftKeyboardDemo
+//
+//  Created by Spencer Williams on 9/8/14.
+//  Copyright (c) 2014 Spencer Williams. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
