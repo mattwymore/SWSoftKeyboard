d17fa2f1172a2223b683b60d44359d8d3f40ed16
Spencer Williams <s@spencerenglish.com>
2014-12-14 16:15:20 -0800

    only inform delegate once per become/resign action

--- SWSoftKeyboard/SWTextField.m
+++ SWSoftKeyboard/SWTextField.m
@@ -8,47 +8,84 @@
 
 #import "SWTextField.h"
 
+#define LOG YES
+
+@interface SWTextField () {
+    BOOL suppressInformingDelegate;
+}
+
+@end
+
 @implementation SWTextField
 @synthesize firstResponderDelegate;
 
 - (BOOL)becomeFirstResponder
 {
-    if (self.firstResponderDelegate &&
+    if (LOG) NSLog(@"[SWTF] become first responder");
+    
+    BOOL informDelegate = !suppressInformingDelegate;
+    suppressInformingDelegate = YES;
+    
+    if (informDelegate &&
+        self.firstResponderDelegate &&
         [self.firstResponderDelegate respondsToSelector:@selector(textFieldWillBecomeFirstResponder:)]) {
+        if (LOG) NSLog(@"  inform delegate of will become");
         [self.firstResponderDelegate textFieldWillBecomeFirstResponder:self];
     }
+    
+    if (LOG) NSLog(@"  super...");
     BOOL didBecomeFirstResponder = [super becomeFirstResponder];
-    if (self.firstResponderDelegate &&
+    
+    if (informDelegate &&
+        self.firstResponderDelegate &&
         [self.firstResponderDelegate respondsToSelector:@selector(textField:didBecomeFirstResponder:)]) {
-        if (didBecomeFirstResponder) {
-            [self.firstResponderDelegate textField:self didBecomeFirstResponder:YES];
-        } else {
-            [self.firstResponderDelegate textField:self didBecomeFirstResponder:NO];
-        }
+        if (LOG) NSLog(@"  inform delegate of did become (%@)", didBecomeFirstResponder ? @"yes" : @"no");
+        [self.firstResponderDelegate textField:self didBecomeFirstResponder:didBecomeFirstResponder];
     }
+    
+    // if suppress was YES before, and we didn't inform the delegate, suppress remains at YES
+    // if suppress was NO before, and we did inform the delegate, suppress is NO again
+    suppressInformingDelegate = !informDelegate;
+    
     return didBecomeFirstResponder;
 }
 - (BOOL)resignFirstResponder
 {
-    if (self.firstResponderDelegate &&
+    if (LOG) NSLog(@"[SWTF] resign first responder");
+    
+    BOOL informDelegate = !suppressInformingDelegate;
+    suppressInformingDelegate = YES;
+    
+    if (informDelegate &&
+        self.firstResponderDelegate &&
         [self.firstResponderDelegate respondsToSelector:@selector(textFieldWillResignFirstResponder:)]) {
+        if (LOG) NSLog(@"  inform delegate of will resign");
         [self.firstResponderDelegate textFieldWillResignFirstResponder:self];
     }
+    
+    if (LOG) NSLog(@"  super...");
     BOOL didResignFirstResponder = [super resignFirstResponder];
-    if (self.firstResponderDelegate &&
+    
+    //resignFirstResponder does not function so you need to resign with this:
+//    [self.windowResponder makeFirstResponder:nil];
+    
+    if (informDelegate &&
+        self.firstResponderDelegate &&
         [self.firstResponderDelegate respondsToSelector:@selector(textField:didResignFirstResponder:)]) {
-        if (didResignFirstResponder) {
-            [self.firstResponderDelegate textField:self didResignFirstResponder:YES];
-        } else {
-            [self.firstResponderDelegate textField:self didResignFirstResponder:NO];
-        }
+        if (LOG) NSLog(@"  inform delegate of did resign (%@)", didResignFirstResponder ? @"yes" : @"no");
+        [self.firstResponderDelegate textField:self didResignFirstResponder:didResignFirstResponder];
     }
+    
+    // if suppress was YES before, and we didn't inform the delegate, suppress remains at YES
+    // if suppress was NO before, and we did inform the delegate, suppress is NO again
+    suppressInformingDelegate = !informDelegate;
+    
     return didResignFirstResponder;
 }
 
 - (void)keyDown:(NSEvent *)theEvent
 {
-    NSLog(@"chars: %@ - unmodchars: %@ - keyCode: %hu", theEvent.characters, theEvent.charactersIgnoringModifiers, theEvent.keyCode);
+    if (LOG) NSLog(@"chars: %@ - unmodchars: %@ - keyCode: %hu", theEvent.characters, theEvent.charactersIgnoringModifiers, theEvent.keyCode);
     [super keyDown:theEvent];
 }
 
